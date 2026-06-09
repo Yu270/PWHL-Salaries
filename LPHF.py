1eb14dbf35d41d3609bb40edafdd0d99bd546fbf
Yu270 <yuhanmenard@gmail.com>
2026-06-08 22:53:47 -0400

    doc: ajuster la docum

--- LPHF.py
+++ LPHF.py
@@ -9,4 +9,4 @@ with st.container():
     st.text("Cette application comprend x pages pour analyser les salaires de base des joueuses de la LPHF.")
     st.text("")
     st.markdown("Sources des données : PWHLPA ([référence](https://www.pwhlpa.com/salary-guide)) et API HockeyTech ([référence](https://github.com/IsabelleLefebvre97/PWHL-Data-Reference))")
-    st.markdown("Récupération et traitement des données : repo [GitHub](https://github.com/Yu270/PWHL-Dashboards/tree/main/data)")
+    st.markdown("Récupération et traitement des données : repo GitHub [PWHL-Dashboards](https://github.com/Yu270/PWHL-Dashboards/tree/main/data)")
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Cette application comprend x pages pour analyser les salaires de base des joueuses de la **Ligue professionnelle de hockey féminin** (LPHF).
 
 Sources des données : PWHLPA ([référence](https://www.pwhlpa.com/salary-guide)) et API HockeyTech ([référence](https://github.com/IsabelleLefebvre97/PWHL-Data-Reference))  
-Récupération et traitement des données : repo [GitHub](https://github.com/Yu270/PWHL-Dashboards/tree/main/data)
+Récupération et traitement des données : repo GitHub [PWHL-Dashboards](https://github.com/Yu270/PWHL-Dashboards/tree/main/data)
