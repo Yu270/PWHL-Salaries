d6e48f544e02fe32cad812c7ed60c8757b6cec79
Yu270 <yuhanmenard@gmail.com>
2026-06-18 20:42:58 -0400

    fct: page par caractéristique

--- LPHF.py
+++ LPHF.py
@@ -8,6 +8,7 @@ with st.container():
     st.subheader("Salaires de la LPHF")
     st.text("Cette application comprend 1 page pour analyser les salaires de base des joueuses de la LPHF.")
     st.markdown("* **Sommaire** : sommaire à haut niveau des salaires de base de la LPHF.")
+    st.markdown("* **Caractéristiques** : répartition des salaires de base de la LPHF selon différentes caractéristiques des joueuses.")
     st.text("")
     st.markdown("Sources des données : PWHLPA ([référence](https://www.pwhlpa.com/salary-guide)) et API HockeyTech ([référence](https://github.com/IsabelleLefebvre97/PWHL-Data-Reference))")
     st.markdown("Récupération et traitement des données : repo GitHub [PWHL-Dashboards](https://github.com/Yu270/PWHL-Dashboards/tree/main/data)")
---
+++ pages/02_Caractéristiques.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import streamlit as st
+from utils.plots import scatter_plot, mean_plot, median_plot
+
+
+st.set_page_config(page_title="Caractéristiques",page_icon="📊")
+
+st.title("Par caractéristique")
+st.text("Cette page présente la répartition des salaires de base de la LPHF selon différentes caractéristiques des joueuses.")
+
+df = pd.read_csv("./data/PWHL_player_salaries_2025-2026.csv")
+df.rename(columns={"Province de naissance": "Province/état de naissance"},inplace=True)
+df["Âge"] = 2026-df["Année de naissance"]
+df["Nombre de joueuses"] = 1
+
+min_games = st.number_input("Nombre minimal de parties jouées lors de la saison régulière 2025-2026",min_value=15,max_value=20,step=1,help="Exclure les joueuses qui ont joué moins de X parties durant la saison régulière 2025-2026.")
+
+df = df[df["Parties jouées"]>=min_games].copy()
+GLOBAL_MEAN = df["Salaire de base 2025-2026"].mean()
+GLOBAL_MEDIAN = df["Salaire de base 2025-2026"].median()
+
+features = {
+    "Âge": {
+        "type": "cont",
+        "comparison": "local",
+        "label_rot": "auto",
+    },
+}
+feature_names = list(features.keys())
+feature_names.sort()
+
+feature = st.selectbox("Caractéristique",options=["Âge"],help="Nom de la caractéristique")
+
+with st.container():
+    st.subheader(f"Distribution du salaire de base 2025-2026 selon {feature}")
+    fig = scatter_plot(df,feature,features[feature]["type"],comparison=features[feature]["comparison"],label_rot=features[feature]["label_rot"])
+    st.pyplot(fig)
+
+with st.container():
+    st.subheader("Salaire moyen selon "+feature)
+    fig = mean_plot(df,feature,features[feature]["type"],comparison=features[feature]["comparison"],label_rot=features[feature]["label_rot"])
+    st.pyplot(fig)
+
+with st.container():
+    st.subheader("Salaire médian selon "+feature)
+    fig = median_plot(df,feature,features[feature]["type"],comparison=features[feature]["comparison"],label_rot=features[feature]["label_rot"])
+    st.pyplot(fig)
