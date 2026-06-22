92b4b80ce962a77dc240081b65d35feff7d9684c
Yu270 <yuhanmenard@gmail.com>
2026-06-21 22:14:42 -0400

    fct: lister les joueuses

--- pages/02_Caractéristiques.py
+++ pages/02_Caractéristiques.py
@@ -7,6 +7,39 @@ from utils.plots import scatter_plot, mean_plot, median_plot
 
 st.set_page_config(page_title="Caractéristiques",page_icon="📊")
 
+@st.fragment
+def players_by_continuous(data: pd.DataFrame, var: str):
+    """
+    Fonction qui affiche la liste des joueuses selon une caractéristique (continue). 
+
+    Entrées
+        data: données à utiliser
+        var: nom de la caractéristique
+    """
+    with st.container(horizontal=True):
+        min_value, max_value = data[var].min(), data[var].max()
+        st.session_state.min_value = st.number_input("Borne inférieure",min_value=min_value,max_value=max_value,help=f"Afficher les joueuses dont {var} est supérieur ou égal à X.")
+        st.session_state.max_value = st.number_input("Borne supérieure",min_value=min_value,max_value=max_value,value=max_value,help=f"Afficher les joueuses dont {var} est inférieur ou égal à X.")
+    temp = data[(data[var]>=st.session_state.min_value)*(data[var]<=st.session_state.max_value)].sort_values("Salaire de base 2025-2026",ascending=False)
+    temp[" "] = range(1,temp.shape[0]+1)
+    temp.set_index(" ",drop=True,inplace=True)
+    st.dataframe(temp[["Nom complet","Équipe",var,"Salaire de base 2025-2026"]],column_config={"Salaire de base 2025-2026": st.column_config.NumberColumn(format="accounting")})
+
+@st.fragment
+def players_by_categorical(data: pd.DataFrame, var: str):
+    """
+    Fonction qui affiche la liste des joueuses selon une caractéristique (catégorique). 
+
+    Entrées
+        data: données à utiliser
+        var: nom de la caractéristique
+    """
+    st.session_state.category = st.multiselect("Valeur(s)",options=np.sort(data[var].unique()),help=f"Afficher les joueuses dont {var} est égale à X.")
+    temp = data[data[var].isin(st.session_state.category)].sort_values("Salaire de base 2025-2026",ascending=False)
+    temp[" "] = range(1,temp.shape[0]+1)
+    temp.set_index(" ",drop=True,inplace=True)
+    st.dataframe(temp[["Nom complet","Équipe",var,"Salaire de base 2025-2026"]],column_config={"Salaire de base 2025-2026": st.column_config.NumberColumn(format="accounting")})
+
 st.title("Par caractéristique")
 st.text("Cette page présente la répartition des salaires de base de la LPHF selon différentes caractéristiques des joueuses.")
 
@@ -48,3 +81,12 @@ with st.container():
     st.subheader("Salaire médian selon "+feature)
     fig = median_plot(df,feature,feature_params["type"],comparison={"mean": GLOBAL_MEAN, "median": GLOBAL_MEDIAN},label_rot=feature_params["label_rot"])
     st.pyplot(fig)
+
+with st.container():
+    st.subheader("Joueuses selon "+feature)
+    if feature_params["type"]=="cont":
+        players_by_continuous(df,feature)
+    elif feature_params["type"]=="cat":
+        players_by_categorical(df,feature)
+    else:
+        st.error("Il y a un problème avec cette caractéristique...")
