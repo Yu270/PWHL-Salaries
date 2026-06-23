9d539c450e117680a814811bea36687abf6ad0a7
Yu270 <yuhanmenard@gmail.com>
2026-06-23 19:26:19 -0400

    fct: page de performances

--- pages/03_Performances.py
+++ pages/03_Performances.py
@@ -1,40 +1,29 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from scipy.stats import pearsonr
 from utils.plots import scatter_plot, mean_plot, median_plot
 
 
 st.set_page_config(page_title="Performances",page_icon="🥅")
 
 @st.fragment
-def players_by_continuous(data: pd.DataFrame, var: str):
+def players_by_continuous(data: pd.DataFrame, var: str, dtype: str):
     """
     Fonction qui affiche la liste des joueuses selon une caractéristique de performance (continue). 
 
     Entrées
         data: données à utiliser
         var: nom de la caractéristique de performance
+        dtype: type de données de la caractéristique de performance ('int' ou 'float')
     """
+    assert dtype in ["int","float"], "dtype doit être 'int' ou 'float'"
+    data[var] = data[var].astype(dtype)
     with st.container(horizontal=True):
         min_value, max_value = data[var].min(), data[var].max()
         st.session_state.min_value = st.number_input("Borne inférieure",min_value=min_value,max_value=max_value,help=f"Afficher les joueuses dont {var} est supérieur ou égal à X.")
         st.session_state.max_value = st.number_input("Borne supérieure",min_value=min_value,max_value=max_value,value=max_value,help=f"Afficher les joueuses dont {var} est inférieur ou égal à X.")
     temp = data[(data[var]>=st.session_state.min_value)*(data[var]<=st.session_state.max_value)].sort_values("Salaire de base 2025-2026",ascending=False)
-    temp[" "] = range(1,temp.shape[0]+1)
-    temp.set_index(" ",drop=True,inplace=True)
-    st.dataframe(temp[["Nom complet","Équipe",var,"Salaire de base 2025-2026"]],column_config={"Salaire de base 2025-2026": st.column_config.NumberColumn(format="accounting")})
-
-@st.fragment
-def players_by_categorical(data: pd.DataFrame, var: str):
-    """
-    Fonction qui affiche la liste des joueuses selon une caractéristique de performance (catégorique). 
-
-    Entrées
-        data: données à utiliser
-        var: nom de la caractéristique de performance
-    """
-    st.session_state.category = st.multiselect("Valeur(s)",options=np.sort(data[var].unique()),help=f"Afficher les joueuses dont {var} est égale à X.")
-    temp = data[data[var].isin(st.session_state.category)].sort_values("Salaire de base 2025-2026",ascending=False)
     temp[" "] = range(1,temp.shape[0]+1)
     temp.set_index(" ",drop=True,inplace=True)
     st.dataframe(temp[["Nom complet","Équipe",var,"Salaire de base 2025-2026"]],column_config={"Salaire de base 2025-2026": st.column_config.NumberColumn(format="accounting")})
@@ -49,47 +38,65 @@ min_games = st.number_input("Nombre minimal de parties jouées lors de la saison
 
 df = df[df["Parties jouées"]>=min_games].copy()
 
+# 'Parties jouées', 'Buts', 'Tirs', 'Mises en échec', 'Tirs bloqués', 'Aides', 'Points', 'Minutes de pénalité', 'Points en AN', 
+# 'Points en DN', 'Mises au jeu', '% de mises au jeu', 'Temps de jeu moyen', 'Minutes pour point', 'Minutes pour tir', 
+# 'Moyenne de buts', 'Moyenne de tirs', 'Moyenne de mises en échec', 'Moyenne de tirs bloqués', 'Moyenne d'aides', 
+# 'Moyenne de points', 'Moyenne de minutes de pénalité', '% de buts', 'Arrêts', 'Buts alloués', 'Jeux blancs', 'Victoires', 
+# 'Moyenne d'arrêts', 'Moyenne de buts alloués', '% d'arrêts', '% de victoires'
 features = {
-    "Position": {
-        "type": "cat",
-        "label_rot": 0,
+    "Parties jouées": {
+        "dtype": "int",
     },
-    "Recrue": {
-        "type": "cat",
-        "label_rot": 0,
+    "Buts": {
+        "dtype": "int",
     },
-    "Numéro": {
-        "type": "cont",
-        "label_rot": "auto",
+    "Tirs": {
+        "dtype": "int",
+    },
+    "Mises en échec": {
+        "dtype": "int",
+    },
+    "Tirs bloqués": {
+        "dtype": "int",
+    },
+    "Aides": {
+        "dtype": "int",
+    },
+    "Points": {
+        "dtype": "int",
+    },
+    "Minutes de pénalité": {
+        "dtype": "int",
+    },
+    "Points en AN": {
+        "dtype": "int",
     },
 }
 feature_names = list(features.keys())
 
 feature = st.selectbox("Caractéristique de performance",options=feature_names,help="Nom de la caractéristique de performance")
-feature_params = features[feature]
-corr_mean = df[df[feature].notna()]["Salaire de base 2025-2026"].mean()
-corr_median = df[df[feature].notna()]["Salaire de base 2025-2026"].median()
+corr_df = df[df[feature].notna()]
+corr_mean = corr_df["Salaire de base 2025-2026"].mean()
+corr_median = corr_df["Salaire de base 2025-2026"].median()
 
 with st.container():
     st.subheader(f"Distribution du salaire de base 2025-2026 selon {feature}")
-    fig = scatter_plot(df[df[feature].notna()],feature,feature_params["type"],comparison={"mean": corr_mean, "median": corr_median},label_rot=feature_params["label_rot"])
+    fig = scatter_plot(corr_df,feature,"cont",comparison={"mean": corr_mean, "median": corr_median},label_rot="auto")
     st.pyplot(fig)
+    r, p = pearsonr(corr_df[feature],corr_df["Salaire de base 2025-2026"])
+    if p<0.05:
+        st.text(f"Coefficient de corrélation linéaire : {r**2:.4f}")
 
 with st.container():
     st.subheader("Salaire moyen selon "+feature)
-    fig = mean_plot(df[df[feature].notna()],feature,feature_params["type"],comparison={"mean": corr_mean, "median": corr_median},label_rot=feature_params["label_rot"])
+    fig = mean_plot(corr_df,feature,"cont",comparison={"mean": corr_mean, "median": corr_median},label_rot="auto")
     st.pyplot(fig)
 
 with st.container():
     st.subheader("Salaire médian selon "+feature)
-    fig = median_plot(df[df[feature].notna()],feature,feature_params["type"],comparison={"mean": corr_mean, "median": corr_median},label_rot=feature_params["label_rot"])
+    fig = median_plot(corr_df,feature,"cont",comparison={"mean": corr_mean, "median": corr_median},label_rot="auto")
     st.pyplot(fig)
 
 with st.container():
     st.subheader("Joueuses selon "+feature)
-    if feature_params["type"]=="cont":
-        players_by_continuous(df[df[feature].notna()],feature)
-    elif feature_params["type"]=="cat":
-        players_by_categorical(df[df[feature].notna()],feature)
-    else:
-        st.error("Il y a un problème avec cette caractéristique...")
+    players_by_continuous(corr_df,feature,features[feature]["dtype"])
